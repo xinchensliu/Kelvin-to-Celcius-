57691a5bd7fe182b388643934564d8fe2dfec461
xinchensliu <106506827+xinchensliu@users.noreply.github.com>
2022-06-26 20:39:38 +1000

    Add files via upload

---
+++ Kelvin weather.java
@@ -0,0 +1,10 @@
+//Kelvin Weather 
+//This is the constant temperature of today 
+const kelvin = 293
+//This is the temp in celcius
+let celcius = 293 - 273
+//temp in fahrenheit 
+let fahrenheit = celcius * (9/5) + 32
+//make not a decimal 
+fahrenheit = Math.floor(fahrenheit)
+console.log('The temperature is ' + fahrenheit + ' degrees Fahrenheit')
